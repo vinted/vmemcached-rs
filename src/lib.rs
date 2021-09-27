da15c575553cd2fa49b654f847043dd5bed70466
Ernestas Poskus <ernestas.poskus@gmail.com>
2021-09-27 19:09:36 +0300

    add: nom ascii parser

--- src/lib.rs
+++ src/lib.rs
@@ -1,74 +1,4 @@
-/*!
-rust-memcache is a [memcached](https://memcached.org/) client written in pure rust.
-
-# Install:
-
-The crate is called `memcache` and you can depend on it via cargo:
-
-```ini
-[dependencies]
-memcache = "*"
-```
-
-# Features:
-
-- <input type="checkbox"  disabled checked /> All memcached supported protocols
-  - <input type="checkbox"  disabled checked /> Binary protocol
-  - <input type="checkbox"  disabled checked /> ASCII protocol
-- <input type="checkbox"  disabled checked /> All memcached supported connections
-  - <input type="checkbox"  disabled checked /> TCP connection
-  - <input type="checkbox"  disabled checked /> UDP connection
-  - <input type="checkbox"  disabled checked/> UNIX Domain socket connection
-  - <input type="checkbox"  disabled checked/> TLS connection
-- <input type="checkbox"  disabled /> Encodings
-  - <input type="checkbox"  disabled checked /> Typed interface
-  - <input type="checkbox"  disabled /> Automatically compress
-  - <input type="checkbox"  disabled /> Automatically serialize to JSON / msgpack etc
-- <input type="checkbox"  disabled checked /> Mutiple server support with custom key hash algorithm
-- <input type="checkbox"  disabled checked /> Authority
-  - <input type="checkbox"  disabled checked /> Binary protocol (plain SASL authority)
-  - <input type="checkbox"  disabled checked /> ASCII protocol
-
-# Basic usage:
-
-```rust
-// create connection with to memcached server node:
-let pool = memcache::Pool::builder()
-  .connection_timeout(std::time::Duration::from_secs(1))
-  .build(memcache::ConnectionManager::new("memcache://127.0.0.1:12345?timeout=10&tcp_nodelay=true").unwrap())
-  .unwrap();
-
-let client = memcache::Client::with_pool(pool);
-
-// flush the database:
-client.flush().unwrap();
-
-// set a string value:
-client.set("foo", "bar", 0).unwrap();
-
-// retrieve from memcached:
-let value: Option<String> = client.get("foo").unwrap();
-assert_eq!(value, Some(String::from("bar")));
-assert_eq!(value.unwrap(), "bar");
-
-// prepend, append:
-client.prepend("foo", "foo").unwrap();
-client.append("foo", "baz").unwrap();
-let value: String = client.get("foo").unwrap().unwrap();
-assert_eq!(value, "foobarbaz");
-
-// delete value:
-client.delete("foo").unwrap();
-
-// using counter:
-client.set("counter", 40, 0).unwrap();
-client.increment("counter", 2).unwrap();
-let answer: i32 = client.get("counter").unwrap().unwrap();
-assert_eq!(answer, 42);
-```
-!*/
-
-#![cfg_attr(feature = "cargo-clippy", allow(clippy::needless_return))]
+//! Vinted Rust memcache
 
 extern crate byteorder;
 extern crate enum_dispatch;
@@ -81,6 +11,7 @@ extern crate url;
 mod client;
 mod connection;
 mod error;
+mod parser;
 mod protocol;
 mod stream;
 mod value;
